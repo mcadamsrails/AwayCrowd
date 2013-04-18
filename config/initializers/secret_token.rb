707ac1a250c542cd550104671e4902336ccd8634
mcadamsrails <mcadamsrails@gmail.com>
2013-04-18 15:28:53 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwayCrowd::Application.config.secret_token = 'e625a47498f11f212bc2e021d9ec55ef87c349f37cee5a2c6c68bbf73c5e05ed5c0755f6df303f047f65419df3bdc4e993fbff3e65ed6fc151b9f4a0e2d5aaf4'
