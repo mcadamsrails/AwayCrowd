9d2a2c50f3ed5dfa7e14eaa39a18a678f885a5f4
Justin McAdams <mcadamsrails@gmail.com>
2013-04-22 22:57:40 -0500

    add some custom css to bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 AwayCrowd::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/about"
+  root :to => 'pages#home'
+
+
+
+  get 'about' => 'pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
